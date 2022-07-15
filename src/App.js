0b7126df983f449a2d99351d7b7084135c2f5f0a
Arthur Reimus <arthurreimus1623@gmail.com>
2022-07-15 20:12:24 +0800

    Test react components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import User from "./components/User";
+import Input from "./components/Input";
 
 function App() {
+  const [user, setUser] = React.useState(null);
+  const [error, setError] = React.useState("");
+  const [counter, setCounter] = React.useState(0);
+  const [inputValue, setInputValue] = React.useState("");
+
+  const increment = () => {
+    setCounter((prevCounter) => (prevCounter += 1));
+  };
+
+  const decrement = () => {
+    setCounter((prevCounter) => (prevCounter -= 1));
+  };
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  React.useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users/1")
+      .then((response) => response.json())
+      .then((data) => setUser(data))
+      .catch((error) => setError(error.message));
+  }, []);
+
+  if (error) {
+    return <span>{error}</span>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
+      <ul className="animals">
+        <li>Cat</li>
+        <li>Whale</li>
+        <li>Lion</li>
+        <li>Elephant</li>
+        <li>Rhino</li>
+      </ul>
+      <Input handleChange={handleChange} inputValue={inputValue} />
+      <div>{user ? <User user={user} /> : <span>Loading...</span>}</div>
+      <h2 data-testid="counter">{counter}</h2>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={increment}>Increment</button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,102 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import { jest } from "@jest/globals";
+import userEvent from "@testing-library/user-event";
+import { toBeInTheDocument } from "@testing-library/jest-dom";
+import App from "./App";
+import Input from "./components/Input";
 
-test('renders learn react link', () => {
+test("renders hello world", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText(/hello world/i)).toBeInTheDocument();
+});
+test("list contains 5 animals", () => {
+  render(<App />);
+  const listElement = screen.getByRole("list");
+  const listItems = screen.getAllByRole("listitem");
+  expect(listElement).toBeInTheDocument();
+  expect(listElement).toHaveClass("animals");
+  expect(listItems.length).toEqual(5);
+});
+
+describe("Testing APP Counter", () => {
+  test("counter is incremented on button click", () => {
+    render(<App />);
+    const counter = screen.getByTestId("counter");
+    const incrementBtn = screen.getByText("Increment");
+
+    userEvent.click(incrementBtn);
+    userEvent.click(incrementBtn);
+    expect(counter.textContent).toEqual("2");
+  });
+
+  test("counter is decremented on button click", () => {
+    render(<App />);
+    const counter = screen.getByTestId("counter");
+    const decrementBtn = screen.getByText("Decrement");
+
+    userEvent.click(decrementBtn);
+    userEvent.click(decrementBtn);
+    expect(counter.textContent).toEqual("-2");
+  });
+});
+
+describe("Test form input", () => {
+  test("input value is updated on input change", () => {
+    render(<App />);
+    const input = screen.getByRole("textbox");
+    userEvent.type(input, "Hello");
+    expect(input.value).toEqual("Hello");
+  });
+
+  test("call the bacllback every time input value is changed", () => {
+    const handleChange = jest.fn();
+    render(<Input handleChange={handleChange} inputValue="" />);
+    const input = screen.getByRole("textbox");
+    userEvent.type(input, "Hello");
+    expect(handleChange).toHaveBeenCalledTimes(5);
+  });
+});
+
+describe("Testing App Component API", () => {
+  test("loading text is shown while fetching data", async () => {
+    window.fetch = jest.fn(() => {
+      const user = { name: "John Doe", email: "john@email.com" };
+
+      return Promise.resolve({
+        json: () => Promise.resolve(user),
+      });
+    });
+    render(<App />);
+    const loading = screen.getByText(/loading/i);
+    expect(loading).toBeInTheDocument();
+
+    await waitForElementToBeRemoved(() => screen.queryByText(/loading/i));
+  });
+
+  test("user is shown after fetching data", async () => {
+    window.fetch = jest.fn(() => {
+      const user = { name: "John Doe", email: "john@email.com" };
+
+      return Promise.resolve({
+        json: () => Promise.resolve(user),
+      });
+    });
+    render(<App />);
+    const userName = await screen.findByText("John Doe");
+    expect(userName).toBeInTheDocument();
+  });
+
+  test("error is shown after fetching data", async () => {
+    window.fetch.mockImplementationOnce(() => {
+      return Promise.reject({ message: "API is down" });
+    });
+
+    render(<App />);
+    const error = await screen.findByText(/down/i);
+    expect(error).toBeInTheDocument();
+  });
 });
